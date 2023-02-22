8c9b7a7401876bdb14708809c00de74a818f9389
artilerist47 <artilerist.sorok.sedmoi@yandex.ru>
2023-02-23 03:47:36 +1000

    hw_27_4 ~ finalv3

--- hm_27_1/urls.py
+++ hm_27_1/urls.py
@@ -34,6 +34,3 @@ urlpatterns = [
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += router.urls
-#
-# if settings.DEBUG:
-#     urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
